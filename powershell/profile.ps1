4b7533cdfba3dbf28c0072923f3d14ea74628699
sky ✩°｡⋆ <atombun@proton.me>
2026-03-04 20:38:35 +0000

    Update profile.ps1
    added git link

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,10 @@
+# https://github.com/PowerShell/PowerShell
+
 #starship
 Invoke-Expression (&starship init powershell)
 
 #fastfetch
 if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
     fastfetch -c "C:/Users/luna/.config/fastfetch/config.jsonc"
+
 }
